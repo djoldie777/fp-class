65cb47a940c75819f6eb34445fd260d300083886
Александр <alexman.p93@gmail.com>
2014-10-21 19:59:27 +0400

    Задание 6

--- class-06/02-textproc.hs
+++ class-06/02-textproc.hs
@@ -1,11 +1,58 @@
+import System.IO
+import Data.Char
+import System.Environment
+import System.Directory
+import System.Random
+
 {-
   Разработайте утилиту со следующими возможностями:
   1) подсчёт количества строк в заданном текстовом файле;
-  2) добавление заданной строки в начало (конец) заданного файла;
+-}
+
+processFile1 :: [FilePath] -> IO ()
+processFile1 (fname : []) = do
+	contents <- readFile fname
+	print $ length $ lines contents
+
+
+--2) добавление заданной строки в начало (конец) заданного файла;
+
+processFile21 :: [FilePath] -> IO ()
+processFile21 (s : fname : []) = do
+	contents <- readFile fname
+	removeFile fname
+	writeFile fname (s ++ "\n" ++ contents)
+
+processFile22 :: [FilePath] -> IO ()
+processFile22 (s : fname : []) = appendFile fname ("\n" ++ s)
+
+
+{-
   3) преобразование всех буквенных символов заданного файла к верхнему
      регистру (результат выводится на консоль);
+-}
+
+processFile3 :: [FilePath] -> IO ()
+processFile3 (fname : []) = do
+	contents <- readFile fname
+	removeFile fname
+	writeFile fname (map toUpper contents)
+	putStrLn $ map toUpper contents
+
+
+{-
   4) построчное слияние двух заданных файлов (каждая строка первого файла
      соединяется с соответствующей строкой второго файла);
+-}
+
+processFile4 :: [FilePath] -> IO ()
+processFile4 (fname1 : fname2 : []) = do
+	contents1 <- readFile fname1
+	contents2 <- readFile fname2
+	writeFile "res.txt" (concat $ map (++ "\n") $ zipWith (++) (lines contents1) (lines contents2))
+
+
+{-
   5) генерация случайного текстового файла (случайность должна ограничиваться
      максимальным количеством строк в файле и символов в строке).
 
@@ -13,4 +60,30 @@
   командной строки.
 -}
 
-main = undefined
+processFile5 :: [[Char]] -> IO ()
+processFile5 (r1 : r2 : []) = do
+	gen1 <- newStdGen
+	gen2 <- newStdGen
+	gen3 <- newStdGen
+	let countLine = head $ (randomRs (1, read r1) gen1 :: [Int])
+	let countChars = head $ (randomRs (1, read r2) gen2 :: [Int])
+	let countAll = countLine * countChars
+	writeFile "random.txt" $ unlines $ fst $ fst $ foldl (\((acc, c), (str, cc)) x -> if (c < cc) then ((acc, c + 1), (str ++ [x], cc)) else ((acc ++ [str], 0), ("", (head $ (randomRs (5, read r2) gen2 :: [Int]))))) (([], 0), ("", countChars)) (take countAll $ (randomRs ('a','z') gen3 :: [Char]))
+
+
+main = do
+	(op : args) <- getArgs
+ 	if (op == "1") then
+		processFile1 args
+	else if (op == "2.1") then
+  		processFile21 args
+	else if (op == "2.2") then
+		processFile22 args
+ 	else if (op == "3") then
+  		processFile3  args
+ 	else if (op == "4") then
+  		processFile4 args
+	else if (op == "5") then
+		processFile5 args
+ 	else
+  		putStrLn "Unknown operation"
--- class-06/03-coords.hs
+++ class-06/03-coords.hs
@@ -1,10 +1,62 @@
+import System.Random
+import System.Environment
+
 {-
   Написать программу, которая в зависимости от параметров командной строки
   а) генерирует случайный текстовый файл содержащий декартовы координаты точек на плоскости
      (по одной точке в каждой строке);
-  б) определяет по заданному файлу в указанном ранее формате количество точек в каждой
-     из четвертей;
-  в) отыскивает наиболее удалённую от начала координат точку.
 -}
 
-main = undefined
+genFile :: [FilePath] -> IO ()
+genFile (fname : []) = do
+	gen1 <- newStdGen
+	gen2 <- newStdGen
+	gen3 <- newStdGen 
+	let countLine = head $ (randomRs (5,10) gen1 :: [Int])
+	writeFile fname $ unlines $ map show $ take countLine $ zip (randomRs (-100,100) gen2 :: [Int]) (randomRs (-100,100) gen3 :: [Int])
+
+
+{-
+  б) определяет по заданному файлу в указанном ранее формате количество точек в каждой
+     из четвертей;
+-}
+
+getQuarter :: (Num a1, Num a2, Num a, Ord a1, Ord a2) => (a1, a2) -> a
+getQuarter (x, y)
+	| x > 0 && y > 0 = 1
+ 	| x < 0 && y > 0 = 2
+ 	| x < 0 && y < 0 = 3
+ 	| otherwise = 4
+
+
+getCount :: [FilePath] -> IO ()
+getCount (fname : []) = do
+	contents <- readFile fname
+	let xs = foldl (\acc x -> acc ++ [getQuarter $ read x]) [] (lines contents)
+	mapM_ print $ foldl (\[q1, q2, q3, q4] x -> if x == 1 then [q1 + 1, q2, q3, q4] 
+		else if x == 2 then [q1, q2 + 1, q3, q4]
+            	else if x == 3 then [q1, q2, q3 + 1, q4]
+            	else [q1, q2, q3, q4 + 1]) [0,0,0,0] xs
+
+
+--в) отыскивает наиболее удалённую от начала координат точку.
+
+getDistance :: Floating a => (a, a) -> a
+getDistance (x, y) = sqrt $ x^2 + y^2
+
+getTheMostRemoted :: [FilePath] -> IO ()
+getTheMostRemoted (fname : []) = do
+	contents <- readFile fname
+	print $ fst $ foldl (\(p, max) x -> if ((getDistance $ read x) > max) then (read x, getDistance $ read x) else (p, max)) ((0,0), 0) (lines contents)
+
+
+main = do
+	(op : args) <- getArgs
+ 	if (op == "1") then
+		genFile args
+	else if (op == "2") then
+  		getCount args
+ 	else if (op == "3") then
+  		getTheMostRemoted  args
+ 	else
+  		putStrLn "Unknown operation"
